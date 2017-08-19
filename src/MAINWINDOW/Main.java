cda4659fc761f5328ca19b09aedbac64db880ccd
MIQUEL <m.galobardes@tesipro.com>
2017-08-19 18:46:09 +0200

    INICIO 2

--- src/MAINWINDOW/Main.java
+++ src/MAINWINDOW/Main.java
@@ -1,10 +1,6 @@
-package MAINWINDOW;
-
 public class Main {
 
-
-    public static void main(String[] args){
-
+    public static void main (String[] args){
         MainFrame mainFrame = new MainFrame();
 
     }
--- src/MAINWINDOW/MainFrame.java
+++ src/MAINWINDOW/MainFrame.java
@@ -1,29 +1,64 @@
-package MAINWINDOW;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class MainFrame {
 
-    private JFrame mainFrame;
-    private JPanel mainPanel;
-    private CardLayout cardLayout;
+    private JFrame Main_frame;
+    private JPanel Main_frame_panel;
+    private JPanel Main_frame_panel_menu;
+    private GridBagLayout Main_frame_gridBagLayout;
+    private GridBagConstraints Main_frame_constraints;
+    private CardLayout Main_panel_card_layout;
 
-    MainFrame(){
+    private JButton button_emails;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+    private JButton button9;
 
-        mainFrame = new JFrame("TESIREPORTS");
-        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        cardLayout = new CardLayout();
+    public MainFrame(){
+
+        Main_frame = new JFrame("TESIREPORTS");
+        Main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Main_panel_card_layout = new CardLayout();
+        Main_frame_gridBagLayout = new GridBagLayout();
+
+        Main_frame_panel = new JPanel(Main_panel_card_layout);
+        Main_frame_panel_menu = new JPanel(Main_frame_gridBagLayout);
+
+        button_emails = new JButton("EMAILS");
 
 
 
 
-        mainPanel = new JPanel(cardLayout);
-        mainFrame.setVisible(true);
+
+        Main_frame_constraints.gridx = 0; // El área de texto empieza en la columna cero.
+        Main_frame_constraints.gridy = 0; // El área de texto empieza en la fila cero
+        Main_frame_constraints.gridwidth = 1; // El área de texto ocupa dos columnas.
+        Main_frame_constraints.gridheight = 1; // El área de texto ocupa 2 filas.
+
+
+        Main_frame_panel_menu.add(button_emails, Main_frame_constraints);
+
+
+        Main_frame_panel.setBackground(Color.CYAN);
+        Main_frame_panel.add(Main_frame_panel_menu);
+
+
+        Main_frame.add(Main_frame_panel);
+        Main_frame.setVisible(true);
 
 
 
 
     }
+
+
+
 
 }
